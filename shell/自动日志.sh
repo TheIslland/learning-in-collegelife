3f54f39b940ffba6e689da048b28a44b59be2c33
TheIsland <861436930@qq.com>
2018-08-17 18:40:30 +0800

    自动日志

--- shell/自动日志.sh
+++ shell/自动日志.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source .back.rc
+source ~/learning-in-collegelife/shell/.back.rc
 if [[ ! -d $dest_dir ]]; then
     echo -e "\033[33m $time_now 【ERROR】the Dest dir $dest_dir not exists \033[0m" >> ~/learning-in-collegelife/log
-    return
+    exit
 fi
-time_now="`date +%Y-%m-%d,%H:%m`"
+time_now="`date +%Y-%m-%d-%H:%M:`"
 echo -e "\033[33m $time_now backup is start \033[0m" >> ~/learning-in-collegelife/log 
 for i in `echo $backup_path | tr ":" "\n"`;do
     if [[ ! -d $i ]]; then
@@ -12,9 +12,9 @@ for i in `echo $backup_path | tr ":" "\n"`;do
     continue
     fi
     cd $i
-    time_now="`date +%Y-%m-%d,%H:%m`"
+    time_new="`date +%Y-%m-%d,%H:%M`"
     way=`pwd`
-    echo -n -e "\033[33m $time_now backup $way \033[0m" >> ~/learning-in-collegelife/log
+    echo -n -e "\033[33m $time_new backup $way \033[0m" >> ~/learning-in-collegelife/log
     n=0
     for k in `du -sh $way`; do
         (( n+=1 ))
@@ -25,9 +25,9 @@ for i in `echo $backup_path | tr ":" "\n"`;do
         echo -e $k >> ~/learning-in-collegelife/log
     done
     target=`echo $i | cut -d "/" -f 2 | tr "/" "_"`
-    target=${target}_${time_now}
+    target=${target}_${time_new}
     tar -czPf /home/mason/learning-in-collegelife/shell/save/${target}.tar.gz $i
-    echo -e -n "\033[33m $time_now delete $way --> ${target}.tar.gz \033[0m" >> ~/learning-in-collegelife/log
+    echo -e -n "\033[33m $time_new delete $way --> ${target}.tar.gz \033[0m" >> ~/learning-in-collegelife/log
     n=0
     for h in `du -sh $way`; do
         (( n+=1 ))
@@ -39,8 +39,8 @@ for i in `echo $backup_path | tr ":" "\n"`;do
     done   
     cd ..
 done
-	Del_list=`find $dest_dir -name "*.tar.gz" -mtime +3`
+	Del_list=`find $dest_dir -name "*.tar.gz" -mmin +3`
 	for i in $Del_list; do
 	rm -f $i
-    	echo -e "\033[33m $time_now delete $i --> remove -${h} \033[0m" >> ~/learning-in-collegelife/log
+    	echo -e "\033[33m $time_new delete $i --> remove -${h} \033[0m" >> ~/learning-in-collegelife/log
 	done 
