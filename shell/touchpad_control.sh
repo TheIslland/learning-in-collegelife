cb4f9405d1092f24fb73cec901e06c7271f6af8d
TheIsland <861436930@qq.com>
2018-08-20 11:03:32 +0800

    touchpad_control

---
+++ shell/touchpad_control.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+alias tpoff="xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Enabled' 0" 
+alias tpon="xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Enabled' 1"
+#write those code in ~/.bashrc 
+#if already saved source ~/.bashrc
