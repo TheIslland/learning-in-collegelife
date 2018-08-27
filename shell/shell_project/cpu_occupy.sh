ea2f1e1071f669cce2f702059937a22791e592d2
TheIsland <861436930@qq.com>
2018-08-28 06:32:13 +0800

    cpu_occupy second test

--- shell/shell_project/cpu_occupy.sh
+++ shell/shell_project/cpu_occupy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+time=`date +%Y-%m-%d__%H:%M:%S` # 时间
 cpu_1=`cat /proc/stat | awk '{if(NR==1) for(i=2;i<=NF;i++)printf("%s ",$i)}'`
-sleep 1
+sleep 0.5
 cpu_2=`cat /proc/stat | awk '{if(NR==1) for(i=2;i<=NF;i++)printf("%s ",$i)}'`
 total_1=`echo $cpu_1 | awk '{for(i=1;i<=NF;i++)sum+=$i}END{printf("%.f",sum)}'`
 total_2=`echo $cpu_2 | awk '{for(i=1;i<=NF;i++)sum+=$i}END{printf("%.f",sum)}'`
@@ -10,5 +11,16 @@ idle_2=`echo $cpu_2 | cut -d " " -f4`
 ((idle=$idle_2 - $idle_1))
 ((cpu_usage=$idle \* 100 / $total ))
 ((cpu_usage=100 - $cpu_usage))
-echo "$cpu_usage"
-
+cpuload=(`cat /proc/loadavg | awk '{print $1,$2,$3}'`)
+load1=${cpuload[0]}
+load5=${cpuload[1]}
+load15=${cpuload[2]}
+cpu_temp=`cat /sys/class/thermal/thermal_zone0/temp | awk '{printf("%d",$1/1000)}'`
+if [[ $cpu_temp -le 50  ]]; then 
+cpu_state="normal"
+elif [[ $cpu_temp -le 70  ]]; then
+cpu_temp="note"
+else
+cpu_state="warning"
+fi
+echo "$time $load1 $load5 $load15 $cpu_usage ${cpu_temp}C $cpu_state"
