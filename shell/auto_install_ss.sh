7db063d5fa63562f9f798e854b5a28d76896d297
TheIsland <861436930@qq.com>
2018-08-28 06:32:51 +0800

    ss

---
+++ shell/auto_install_ss.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+yum install vim-enhanced
+yum -y install epel-release
+yum -y install python-pip
+yum clean all
+pip install --upgrade pip
+pip install shadowsocks
+wget -c https://raw.githubusercontent.com/TheIslland/learning-in-collegelife/master/shell/shdowsocks.json
+mv shdowsocks.json /etc
+wget -c https://raw.githubusercontent.com/TheIslland/learning-in-collegelife/master/shell/shadowsocks.service
+mv shadowsocks.service /etc/systemd/system
+systemctl enable shadowsocks
+systemctl start shadowsocks
+systemctl status shadowsocks -l
+firewall-cmd --state 
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+firewall-cmd --state
+
