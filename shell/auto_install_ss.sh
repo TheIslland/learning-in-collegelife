2168d70498a7d3037f9cb8a77e2bf6c7cdf95254
TheIsland <861436930@qq.com>
2018-08-29 07:48:38 +0800

    correct

--- shell/auto_install_ss.sh
+++ shell/auto_install_ss.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-yum install vim-enhanced
+yum -y install vim-enhanced
 yum -y install epel-release
 yum -y install python-pip
 yum clean all
-pip install --upgrade pip
-pip install shadowsocks
+pip -y install --upgrade pip
+pip -y install shadowsocks
 wget -c https://raw.githubusercontent.com/TheIslland/learning-in-collegelife/master/shell/shdowsocks.json
 mv shdowsocks.json /etc
 wget -c https://raw.githubusercontent.com/TheIslland/learning-in-collegelife/master/shell/shadowsocks.service
