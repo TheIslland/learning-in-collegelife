2adda418e98d4940c73d41166411a03361c27598
TheIsland <861436930@qq.com>
2018-08-28 08:08:17 +0800

    correct

--- shell/shell_project/MEM.sh
+++ shell/shell_project/MEM.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 time=`date +%Y-%m-%d__%H:%M:%S`
-total=`free -m | awk '{if(NR==2) printf("%s %s",$2,$3)}'`
-#used=`free -m | awk '{if(NR==2) print $3}'`
-(( use=${total[0]}-${total[1]}))
-(( zhanyong=use*100/${total[0]} ))
-echo -n "$time ${total[0]} ${total[1]} $zhanyong% "
-echo "scale=1;0.3*$1+0.7$zhanyong" | bc
+MEM_total=`free -m | awk '{if(NR==2) printf("%s %s",$2,$3)}'`
+MEM_used=`echo ${MEM_total[0]} | cut -d " " -f2`
+MEM_total=`echo ${MEM_total[0]} | cut -d " " -f1`
+(( MEM_use=$MEM_total-$MEM_used ))
+MEM_proportion=`echo "scale=2;$MEM_use*100/$MEM_total" | bc`
+echo -n "$time $MEM_total $MEM_used $MEM_proportion% "
+echo "scale=1;0.3*$1+0.7*$MEM_proportion" | bc
 
