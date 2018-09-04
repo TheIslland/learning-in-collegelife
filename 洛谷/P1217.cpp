718e919a42cc86037abc8d64d0d772cf7eeff673
mason <41140112+TheIslland@users.noreply.github.com>
2018-09-04 20:16:50 +0800

    Create P1217.cpp

---
+++ 洛谷/P1217.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+#define maxn 9989899+1
+bool Prime[maxn+1];
+int ans[800];
+void getPrime()
+{
+    memset(Prime,true,sizeof(Prime));
+    for (int i=2;i<maxn ;i++ )
+    {
+        if(Prime[i])
+        {
+                for (int j=i+i;j<maxn ;j+=i )
+            {
+                Prime[j]=false;
+            }
+        }
+    }
+}
+bool Huiwen(int n)
+{
+    int sum=0,nn=n;
+    while (n)
+    {
+        sum=sum*10+n%10;
+        n/=10;
+    }
+    return sum==nn;
+}
+int main()
+{
+   int a,b;
+   getPrime();
+   int k=0;
+   for(int i=2;i<maxn;i++)
+   {
+       if(Prime[i]&&Huiwen(i))
+        ans[k++]=i;
+   }
+   while (cin>>a>>b)
+   {
+        for (int i=0;i<=k ;i++ )
+        {
+            if(ans[i]<a)
+                continue;
+            else if(ans[i]>b)
+                break;
+            else
+                cout<<ans[i]<<endl;
+        }
+        cout<<endl;
+   }
+
+    return 0;
+}
