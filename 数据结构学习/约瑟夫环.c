6b144283fe58e593bdc945ada72eefd909e910bb
TheIsland <861436930@qq.com>
2018-09-10 19:46:01 +0800

    约瑟夫环.c

---
+++ 数据结构学习/约瑟夫环.c
@@ -0,0 +1,72 @@
+/*************************************************************************
+	> File Name: 约瑟夫环.c
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2018年09月10日 星期一 19时45分21秒
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+}Node, *LinkedList;
+
+LinkedList insert(LinkedList head, Node *node, int index) {
+    if (head == NULL) {
+        if (index != 0) {
+            return head;
+        }
+        head = node;
+        head->next = head;
+        return head;
+    }
+    if (index == 0) {
+        node->next = head->next;
+        head->next = node;
+        return head;
+    }
+    Node *current_node = head->next;
+    int count = 0;
+    while (current_node != head && count < index - 1) {
+        current_node = current_node->next;
+        count++;
+    }
+    if (count == index - 1) {
+        node->next = current_node->next;
+        current_node->next = node;
+    }
+    if (node == head->next) {
+        head = node;
+    }
+    return head;
+}
+
+void output_josephus (LinkedList head, int m) {
+    Node *current_node = head;
+    head = NULL;
+    while (current_node->next != current_node) {
+		for (int i = 1; i < m; i++) {
+            current_node = current_node->next;
+        }
+        printf("%d ", current_node->next->data);
+        Node *delete_node = current_node->next;
+        current_node->next = current_node->next->next;
+        free(delete_node);
+    }
+    printf("%d\n", current_node->data);
+    free(current_node);
+}
+int main() {
+    LinkedList linkedlist = NULL;
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++) {
+        Node *node = (Node *)malloc(sizeof(Node));
+        node->data = i;
+        node->next = NULL;
+        linkedlist = insert(linkedlist, node, i - 1);
+    }
+    output_josephus(linkedlist, m);
+    return 0;
+}
