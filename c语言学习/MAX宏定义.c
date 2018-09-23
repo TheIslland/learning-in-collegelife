1df9e4ed2c4c58d22e2c36ae2e433434afa0ef55
TheIsland <861436930@qq.com>
2018-09-23 14:07:33 +0800

    "MAX\345\256\217\345\256\232\344\271\211.c"

---
+++ c语言学习/MAX宏定义.c
@@ -0,0 +1,30 @@
+/*************************************************************************
+	> File Name: MAX宏定义.c
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2018年09月23日 星期日 11时58分56秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define P(func) {\
+    printf("%s = %d\n", #func, func); \
+}
+
+#define MAX(a, b) ({\
+    __typeof(a) x = a;\
+    __typeof(b) y = b;\
+    (x) > (y) ? (x) : (y);\
+})
+int main() {
+    P(MAX(2, 3));
+    P(5 + MAX(2, 3));
+    P(MAX(2, MAX(3, 4)));
+    P(MAX(2, 3 > 4 ? 3 : 4));
+    int a = 7;
+    P(MAX(a++, 6));
+    printf("a = %d\n", a);
+    return 0;
+}
