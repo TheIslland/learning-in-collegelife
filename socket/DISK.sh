e8422a6dd29f30b5bb59439bcccd39b07fef1f24
TheIsland <861436930@qq.com>
2018-09-27 22:39:27 +0800

    监控日志配套脚本

---
+++ socket/DISK.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+time=`date +%Y-%m-%d__%H:%M:%S` # 时间
+information=(`df -T -x devtmpfs -m -x tmpfs | tail -n +2 | awk '{printf("a["NR"]=%s,%d,%d,%s ",$7,$3,$5,$6)}'`) # 磁盘分区信息
+disk_total=0 # 磁盘内存总量
+used_disk=0 # 磁盘可用内存
+((len=${#information[@]}-1)) # 分区总数
+for((i=0;i<=len;i++)); do
+    ((disk_total=$disk_total+`echo ${information[$i]} | cut -d "," -f 2`)) #磁盘总量
+    ((used_disk=$used_disk+`echo ${information[$i]} | cut -d "," -f 3`)) #磁盘可用内存
+done
+((proportion=($disk_total-$used_disk)*100/$disk_total)) #占用比
+echo "$time 0 disk $disk_total $used_disk $proportion%" #磁盘信息
+for((j=0;j<=len;j++)); do
+    echo "$time 1 `echo ${information[j]} | cut -d "=" -f 2 | tr "," " "`" #分区信息
+done
---
+++ socket/cpu_occupy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+time=`date +%Y-%m-%d__%H:%M:%S` # 时间
+cpu_1=`cat /proc/stat | awk '{if(NR==1) for(i=2;i<=NF;i++)printf("%s ",$i)}'`
+sleep 0.5
+cpu_2=`cat /proc/stat | awk '{if(NR==1) for(i=2;i<=NF;i++)printf("%s ",$i)}'`
+total_1=`echo $cpu_1 | awk '{for(i=1;i<=NF;i++)sum+=$i}END{printf("%.f",sum)}'`
+total_2=`echo $cpu_2 | awk '{for(i=1;i<=NF;i++)sum+=$i}END{printf("%.f",sum)}'`
+idle_1=`echo $cpu_1 | cut -d " " -f4`
+idle_2=`echo $cpu_2 | cut -d " " -f4`
+((total=$total_2 - $total_1))
+((idle=$idle_2 - $idle_1))
+cpu_usage=`echo "scale=2;$idle * 100 / $total" | bc`
+cpu_usage=`echo "100 - $cpu_usage" | bc`
+cpuload=(`cat /proc/loadavg | awk '{print $1,$2,$3}'`)
+load1=${cpuload[0]}
+load5=${cpuload[1]}
+load15=${cpuload[2]}
+cpu_temp=`cat /sys/class/thermal/thermal_zone0/temp | awk '{printf("%.2f",$1/1000)}'`
+if [[ $(echo "$cpu_temp <= 50" | bc)=1 ]]; then 
+cpu_state="normal"
+elif [[ $(echo "$cpu_temp <= 70" | bc)=1 ]]; then
+cpu_state="note"
+else
+cpu_state="warning"
+fi
+echo "$time $load1 $load5 $load15 $cpu_usage ${cpu_temp}C $cpu_state"
