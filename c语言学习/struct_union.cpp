6d7b859047c5315e184b0e10bd10f65729a1d364
TheIsland <861436930@qq.com>
2018-10-04 15:20:59 +0800

    struct_union.cpp

--- c语言学习/struct_union.cpp
+++ c语言学习/struct_union.cpp
@@ -7,11 +7,13 @@
 
 #include <iostream>
 #include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 #include <string>
 #include <cstdio>
-//#pragma pack()
-//改变存储单元对齐格式
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct person {
     char name[20];
@@ -21,15 +23,63 @@ struct person {
 };
 
 struct test {
-    short a;   //short两个两个读
-    char b; //一个一个读
-    int c; //只能4个4个读，为保证正确性和完整性从4开始存
-    double e; 
+    char b;
+    short a;
+    int c;
+    double e;
 };
 
+struct Number {
+    int type; // 0->int or 1->double
+    union {
+        int num1;
+        float num2;
+    } N;
+};
+
+void print(Number *n) {
+    switch (n->type) {
+        case 0: printf("%d\n", n->N.num1); break;
+        case 1: printf("%f\n", n->N.num2); break;
+    }
+    return ;
+}
+
+void set(Number *n, float num) {
+    n->type = 1;
+    n->N.num2 = num;
+}
+
+void set(Number *n, int num) {
+    n->type = 0;
+    n->N.num1 = num;
+}
+
 int main() {
+    srand(time(0));
     struct test a;
-    printf("sizeof(person) : %d\n", sizeof(struct person)); 
-    printf("%p %p %p %p\n", &a.a, &a.b, &a.c, &a.e);
+    printf("sizeof(person) : %d\n", sizeof(struct person));
+    printf("%p %p %p", &a.a, &a.b, &a.c);
+    #define MAX_N 20
+    Number arr[MAX_N];
+    for (int i = 0; i < MAX_N; i++) {
+        int op = rand() % 2;
+        switch (op) {
+            case 0: {
+                int value = rand() % 100;
+                set(arr + i, value);
+            } break;
+            case 1: {
+                float value = (1.0 * (rand() % 10000) / 10000) * 100;
+                set(arr + i, value);
+            } break;
+        }
+    }
+    for (int i = 0; i < MAX_N; i++) {
+        print(arr + i);
+    }
+    arr[0].N.num2 = 1.0;
+    printf("arr[0].N.num1 = %d\n", arr[0].N.num1);
+    printf("arr[0].N.num1 = %x\n", arr[0].N.num1);
     return 0;
 }
