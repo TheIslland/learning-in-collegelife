54ba80a2c7ebe4c8dd5b2e370ffe7cab2298c155
TheIsland <861436930@qq.com>
2018-10-17 20:33:33 +0800

    auto_number.sh

---
+++ auto_number.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+fil_num=1
+dir_num=1
+for i in `ls`; do
+    if [[ $i == "a.out" ]]; then
+        continue
+    fi
+    if [[ -d $i ]]; then
+       mv $i ${dir_num}.${i}
+       (( dir_num=${dir_num}+1 ))
+    fi
+    if [[ -f $i ]]; then
+        mv $i ${fil_num}.${i}
+        (( fil_num=${fil_num}+1 ))
+    fi
+done
