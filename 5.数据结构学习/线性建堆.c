dbcdf2b57c63b0809c734d74099f4e82c01d0a28
TheIsland <861436930@qq.com>
2018-10-30 21:01:34 +0800

    线性建堆.c

---
+++ 5.数据结构学习/线性建堆.c
@@ -0,0 +1,65 @@
+/*************************************************************************
+	> File Name: 线性建堆.c
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2018年10月30日 星期二 20时38分28秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define swap(a, b) {\
+    __typeof(a) __temp = a; a = b; b = __temp;}
+typedef struct Heap{
+    
+} Heap;
+
+void heap_sort(int *arr, int n) {
+    int *p = arr - 1;
+    for (int i = n >> 1; i >= 1; i--) {
+        int ind = i;
+        while ((ind << 1) <= n) {
+            int temp = ind;
+            if (p[temp] < p[ind << 1]) temp = ind << 1;
+            if ((ind << 1 | 1) <= i - 1 && p[temp] < p[ind << 1 | 1]) temp = ind << 1 | 1;
+            if (temp == ind) break;
+            swap(p[temp], p[ind]);
+            ind = temp;
+        }
+    }
+    for (int i = n; i > 1; i--) {
+        swap(p[1], p[i]);
+        int ind = 1;
+        while ((ind << 1) <= i - 1) {
+            int temp = ind;
+            if (p[temp] < p[ind << 1]) temp = ind << 1;
+            if ((ind << 1 | 1) <= i - 1 && p[temp] < p[ind << 1 | 1]) temp = ind << 1 | 1;
+            if (temp == ind) break;
+            swap(p[temp], p[ind]);
+            ind = temp;
+        }
+    }
+    return ;
+}
+
+void output(int *num, int n) {
+    printf("ARR = [");
+    for (int i = 0; i < n; i++) {
+        printf(" %d", num[i]);
+        i == n - 1 || printf(",");
+    }
+    printf("]\n");
+    return ;
+}
+int main() {
+    #define MAX_N 20 
+    srand(time(0));
+    int arr[MAX_N];
+    for (int i = 0; i < MAX_N; i++) arr[i] = rand() % 100;
+    output(arr, MAX_N);
+    heap_sort(arr, MAX_N);
+    output(arr, MAX_N);
+    return 0;
+}
