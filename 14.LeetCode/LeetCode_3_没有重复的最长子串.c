a235012ece7b7df544cdab6226ccfdc0c133c1e5
TheIsland <861436930@qq.com>
2018-11-08 19:05:03 +0800

    LeetCode_3_没有重复的最长子串.c

--- 14.LeetCode/LeetCode_3_没有重复的最长子串.c
+++ 14.LeetCode/LeetCode_3_没有重复的最长子串.c
@@ -17,4 +17,15 @@ int lengthOfLongestSubstring(char* s) {
     }
     return ans;
 }
- 
+
+int lengthOfLongestSubstring(char *s) {
+    int pos[256] , len = 0;
+    memset(pos, -1, sizeof(pos));
+    for (int i = 0; s[i]; i++) {
+        len += 1;
+        if (i - pos[s[i]] < len) len = i - pos[s[i]];
+        pos[s[i]] = i;
+        if (len > ans) ans = len;
+    }
+    return ans;
+}
