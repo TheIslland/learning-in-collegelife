2ef34f20fc05e25b5262816ff12a69fd9cb52476
TheIsland <861436930@qq.com>
2018-12-23 14:49:02 +0800

    client.c

--- 12.项目/3.分布式服务器监控系统/master/client.c
+++ 12.项目/3.分布式服务器监控系统/master/client.c
@@ -38,6 +38,9 @@ void func_w(char *m, int sockfd) {
     FILE *fp = popen(m, "r");
     fread(data, sizeof(char), sizeof(data), fp);
     strncpy(buf, data, strlen(data));
+    if (buf[0] == '\0') {
+        return ;
+    }
     while (send(sockfd, buf, strlen(buf), 0) < -1);
     pclose(fp);
     return ;
@@ -64,36 +67,80 @@ void *func1(void *argv) {
     while (1) {
       while (connect(fd, (struct sockaddr*)&server, sizeof(struct sockaddr)) == -1);
         func_w("./warning_monitoring.sh", fd);        
+        func_w("./malicious_process_detection.sh", fd);
         if (fd != -1) close(fd);
         printf("cds\n");
         sleep(10);
     }
 }
 
-void *soc_func(void *argv) {
+void *alert_func(void *argv) {
     struct sockaddr_in server;
     server.sin_family = AF_INET; // 初始化服务器地址
-    server.sin_port = htons(8432);
+    server.sin_port = htons(8536);
     server.sin_addr.s_addr = inet_addr("192.168.2.163");
     bzero(&(server.sin_zero), 8);
-    int fd;
-    while ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1); // socket
-    while (1) {
-      while (connect(fd, (struct sockaddr*)&server, sizeof(struct sockaddr)) == -1);
-        if (fd != -1) close(fd);
-        printf("cds\n");
-        sleep(5);
+    int fd, sockfd;
+	
+	while (1) {
+		char buf[BUFSIZ] = {0};
+		buf[0] = '1';
+		char data[BUFSIZ] = {0};
+        int flag = 0;
+		FILE *fp = popen("./System_operation.sh", "r");
+		fread(data, sizeof(char), sizeof(data), fp);
+		char *str = strstr(data, "note");
+		char *str2 = strstr(data, "waring"); 
+		if(str || str2) {
+			strncpy(buf + 1, data, strlen(data));
+            flag = 1;
+		}
+		pclose(fp);
+        memset(data, 0, sizeof(data));
+		FILE *fp2 = popen("./malicious_process_detection.sh", "r");
+		fread(data, sizeof(char), sizeof(data), fp);
+		if(data[0]) {
+			strncpy(buf + strlen(buf), data, strlen(data));
+            flag = 1;
+		}
+		printf("%s\n", buf);
+		pclose(fp2);
+        if (flag) {
+			buf[strlen(buf)] = '\n';
+			buf[strlen(buf)] = 0;
+		    while ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1); // socket
+	        while ((sockfd = connect(fd, (struct sockaddr*)&server, sizeof(struct sockaddr))) == -1);
+		    send(fd, buf, strlen(buf), 0);
+        }
+		if (fd != -1) close(fd);
     }
     return NULL;
 }
 
+
+void *soc_func(void *argv) {
+    struct sockaddr_in server;
+    server.sin_family = AF_INET; // 初始化服务器地址
+    server.sin_port = htons(8765);
+    server.sin_addr.s_addr = inet_addr("192.168.2.163");
+    bzero(&(server.sin_zero), 8);
+    int fd;
+    while ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1); // socket
+    while (connect(fd, (struct sockaddr*)&server, sizeof(struct sockaddr)) == -1);
+    if (fd != -1) close(fd);
+    printf("cds\n");
+    pthread_exit(0);
+}
+
 int main(int argc, char *argv[]) {
+    /*
     if (argc < 3) {
         printf("./Client 地址 端口\n");
         exit(1);
     }
     int port = atoi(argv[2]); // int型 端口
     char *host = argv[1]; // 地址
+    */
     int fd, new_fd, struct_len, numbytes, i;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
@@ -106,7 +153,7 @@ int main(int argc, char *argv[]) {
     struct_len = sizeof(struct sockaddr_in); 
     struct sockaddr_in *ser = &server_addr;
     pthread_t t, w;
-    pthread_create(&w, NULL, func1, NULL);
+    pthread_create(&w, NULL, alert_func, NULL);
     pthread_create(&t, NULL, soc_func, NULL);
     fd = socket(AF_INET, SOCK_STREAM, 0); // socket
     while(bind(fd, (struct sockaddr *)&server_addr, struct_len) == -1); // bind 绑定
@@ -119,7 +166,9 @@ int main(int argc, char *argv[]) {
         sleep(1);
         func("mem.log", "./MEM.sh", new_fd);
         func("disk.log", "./DISK.sh", new_fd);
+        func("User.log", "./User_information_statistics.sh", new_fd);
         sleep(5);
+        func("bad_proccess.log", "./malicious_process_detection.sh", new_fd);
     }
     return 0;
 }
