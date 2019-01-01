679f0a8d48726ef7ae8f8695c853bef659b10c3d
TheIsland <861436930@qq.com>
2019-01-01 19:49:20 +0800

    构造函数.cpp

---
+++ 18.c++/构造函数.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+	> File Name: 构造函数.cpp
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2019年01月01日 星期二 19时48分44秒
+ ************************************************************************/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+class Clock{
+public:
+    Clock(){
+        second = 0;
+        minute = 0;
+        hour = 0;
+    }
+    Clock(int newS, int newM, int newH);
+    void setTime(int newS,int newM,int newH){
+        second=newS;
+        minute=newM;
+        hour=newH;
+    }
+    void showTime();
+private:
+    int second,minute,hour;
+};
+void Clock::showTime(){
+    cout << hour << ":" << minute << ":" << second << endl;
+}
+
+Clock::Clock(int newS, int newM, int newH){
+    second=newS;
+    minute=newM;
+	hour=newH;
+}
+int main(){
+    Clock MyClock;
+    MyClock.showTime();
+    int second,minute,hour;
+    cin >> second >> minute >> hour;
+    MyClock.setTime(second,minute,hour);
+    MyClock.showTime();
+    Clock MyClock2(1, 2, 3);
+    MyClock2.showTime();
+    return 0;
+}
+
