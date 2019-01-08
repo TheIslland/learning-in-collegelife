b710cd222daee180a57e96e3f10b0ee218fe0982
TheIsland <861436930@qq.com>
2019-01-08 18:58:24 +0800

    c++返回值优化.cpp

---
+++ 18.c++/c++返回值优化.cpp
@@ -0,0 +1,53 @@
+/*************************************************************************
+	> File Name: c++返回值优化.cpp
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2019年01月08日 星期二 18时16分18秒
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Test {
+private:
+    int i;
+public:
+    Test(int v) {
+        i = v;
+        cout << "Test(int v) : i = " << i << endl;
+    }
+    Test() {
+        //Test(100);
+        //第２３行会产生一个临时对象（幂名），生命周期只有一行要尽量避免使用
+        i = 0;
+        cout << "Test() : i = " << i << endl;
+    }
+    Test(const Test &t) {
+        i = t.i; // ?
+        cout << "Test(const Test &t) : i = " << i << endl;
+    }
+    void printI() {
+        cout << "i = " << i << endl;
+    }
+};
+
+void func1(Test t) {
+    return ;
+}
+
+Test func2() {
+    return Test(100); //生成临时对象返回时发生一次拷贝
+    //return Test(100);
+}
+
+int main() {
+    Test t = Test(100); //等价于t = 100
+    t.printI();
+    Test t1 = func2(); // 发生一次拷贝
+    func1(t); //有拷贝构造函数调用
+    return 0;
+}
