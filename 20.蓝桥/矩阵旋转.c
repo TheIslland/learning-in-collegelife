2dd405afcd27ac2aa1c2b3244b1aba17e0d3579a
TheIsland <861436930@qq.com>
2019-01-10 10:34:46 +0800

    矩阵旋转.c

---
+++ 20.蓝桥/矩阵旋转.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 矩阵旋转.c
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2019年01月10日 星期四 10时34分26秒
+ ************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a[200][200] = {0};
+    for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &a[j][n - i - 1]);
+        }
+    }
+    for (int i = 0 ; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d", a[i][j]);
+            if (j < n -1) printf(" ");
+        }
+        printf("\n");
+    }
+	return 0;
+}
