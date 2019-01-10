cdb86e5d25b1ee59c96d091ecc0c2f62813ada7e
TheIsland <861436930@qq.com>
2019-01-10 14:24:41 +0800

    四平方和.c

---
+++ 20.蓝桥/四平方和.c
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: 四平方和.c
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2019年01月10日 星期四 14时24分26秒
+ ************************************************************************/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[4] = {0};
+    int temp = 0, ans = 0;
+	for (int i = 0; i * i < n; i++) {
+        int flag = 0;
+        for (int j = 0; j * j < n; j++) {
+            if (n - i * i - j * j <= 0) continue;
+			for (int k = 0; k * k < n; k++) {
+                if (n - i * i - j * j - k * k <= 0) continue;
+				double temp = sqrt(n - i * i - j * j - k * k);
+                if (temp == (int)temp) {
+                    flag = 1;
+                    cout << i << " " << j << " " << k << " " << temp;
+                    break;
+                }
+            }
+            if (flag) break;
+        }
+        if (flag) break;
+    }
+	return 0;
+}
