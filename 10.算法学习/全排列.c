0374b51d877bbca54fab0ec3762ffb7a1e76a1a9
TheIsland <861436930@qq.com>
2019-01-12 10:28:09 +0800

    全排列.c

--- 10.算法学习/全排列.c
+++ 10.算法学习/全排列.c
@@ -9,31 +9,33 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-void swap(char *a, char *b) {
+
+//以下全排列函数的非递归实现
+void swap(char *a, char *b) { //交换函数
     char temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void reverse(char *a, char *b) {
+void reverse(char *a, char *b) { //数组翻转
     while (a < b) {
         swap(a++, b--);
     }
 }
 
 int next_permutation(char *first, char *end) {
-    if (first == end) return 0;
-    char *p = end;
+    if (first == end) return 0; // 如果数组首尾指向同一地方则无需翻转已全排列（单个元素）
+    char *p = end; 
     char *q = p;
     while (p != first) {
         p--;
         char *find = end;
-        if (*p < *q) {
-            while (*find <= *p) {
+        if (*p < *q) { //找到第一个非有序递减的数的位置ｐ
+            while (*find <= *p) { //找到第一个比*p大的数
                 find--;
             }
-            swap(p, find);
-            reverse(p + 1, end);
+            swap(p, find); //将p与find的值交换
+            reverse(p + 1, end); //翻转p后到末尾的元素保证字典序输出
             return 1;
         }
         q--;
