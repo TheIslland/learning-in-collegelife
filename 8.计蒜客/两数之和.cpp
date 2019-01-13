6368b153e4cbf737a75ca37fccd251c0218a1bb7
TheIsland <861436930@qq.com>
2019-01-13 16:01:35 +0800

    两数之和.cpp

--- 8.计蒜客/两数之和.cpp
+++ 8.计蒜客/两数之和.cpp
@@ -4,42 +4,34 @@
 	> Mail: 861436930@qq.com
 	> Created Time: 2019年01月13日 星期日 15时35分36秒
  ************************************************************************/
-
 #include <iostream>
-#include <algorithm>
-#include <string>
 #include <cstdio>
-using std::cin;
-using std::cout;
-using std::endl;
+#include <cstdlib>
+
+using namespace std;
+
 typedef struct Data {
     int val, pos;
-}Data;
-typedef struct Hashtable {
+} Data;
+
+typedef struct HashTable {
     Data *data;
     char *flag;
     int n;
-} Hashtable;
+} HashTable;
 
-Hashtable *init(int n) {
-    Hashtable *h = (Hashtable *)malloc(sizeof(Hashtable));
+HashTable *init(int n) {
+    HashTable *h = (HashTable *)malloc(sizeof(HashTable));
     h->data = (Data *)malloc(sizeof(Data) * n);
     h->flag = (char *)calloc(sizeof(char), n);
     h->n = n;
-    return h;
-}
-void clear(Hashtable *h) {
-    if (h == NULL) return ;
-    free(h->data);
-    free(h);
-    return ;
 }
 
-int hash_func(Hashtable *h, int val) {
+int hash_func(HashTable *h, int val) {
     return val % h->n;
 }
 
-void insert(Hashtable *h, int val, int ind) {
+void insert(HashTable *h, int val, int ind) {
     int hash = hash_func(h, val);
     int time = 1;
     while (h->flag[hash]) {
@@ -52,7 +44,7 @@ void insert(Hashtable *h, int val, int ind) {
     return ;
 }
 
-int query(Hashtable *h, int val) {
+int query(HashTable *h, int val) {
     int hash = hash_func(h, val);
     int time = 1;
     while (h->flag[hash] && h->data[hash].val != val) {
@@ -63,26 +55,33 @@ int query(Hashtable *h, int val) {
     return h->data[hash].pos;
 }
 
+void clear(HashTable *h) {
+    if (h == NULL) return ;
+    free(h->data);
+    free(h);
+    return ;
+}
 
 #define MAX_N 500
 int arr[MAX_N + 5] = {0};
-
 int main() {
     int n, target;
     scanf("%d", &n);
-    Hashtable *h = init(3 * n);
-    for (int i = 0; i < n; i++) {
+    HashTable *h = init(3 * n);
+    for (int i = 1; i <= n; i++) {
         scanf("%d", arr + i);
     }
     scanf("%d", &target);
     for (int i = 1; i <= n; i++) {
         int pos;
-        if ((pos = query(h, target  -arr[i])) == -1) {
+        if ((pos == query(h, target - arr[i])) == -1) {
             insert(h, arr[i], i);
             continue;
         }
         printf("%d %d\n", pos, i);
+        break;
     }
+
     clear(h);
     return 0;
 }
