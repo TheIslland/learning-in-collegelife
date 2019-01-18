1729a68b5e4f94685edb76f5e254505c86e0b0d4
TheIsland <861436930@qq.com>
2019-01-18 19:04:56 +0800

    操作符重载.cpp

--- 18.c++/操作符重载.cpp
+++ 18.c++/操作符重载.cpp
@@ -16,35 +16,85 @@ using namespace std;
 
 class Complex {
 private: 
-    int real;
-    int imag;
+    double real;
+    double imag;
 public:
     Complex(int r = 0, int i = 0) {
         real = r;
         imag = i;
     }
-    int getR() {
+    double getR() {
         return this->real;
     }
-    int getI() {
+    double getI() {
         return this->imag;
     }
-    friend Complex operator+(const Complex &c1, const Complex &c2);
+    friend Complex& operator+(const Complex &c1, const Complex &c2);
+    friend Complex& operator-(const Complex &c1, const Complex &c2);
+    friend Complex& operator*(const Complex &c1, const Complex &c2);
+    friend Complex& operator/(const Complex &c1, const Complex &c2);
+    friend bool operator==(const Complex &c1, const Complex &c2);
+    friend bool operator!=(const Complex &c1, const Complex &c2);
+    Complex& operator=(const Complex &c1) {
+        if(this != &c1) { //不润徐自赋值　a = a
+            this->real = c1.real;
+            this->imag = c1.imag;
+        }
+        return *this;
+    }
 };
 
-Complex operator+(const Complex &c1, const Complex &c2) {
-    Complex ret;
-    ret.real = c1.real + c2.real;
-    ret.imag = c1.imag + c2.imag;
-    return ret;
+Complex& operator+(const Complex &c1, const Complex &c2) {
+    Complex *ret;
+    ret->real = c1.real + c2.real;
+    ret->imag = c1.imag + c2.imag;
+    return *ret;
+}
+Complex& operator-(const Complex &c1, const Complex &c2) {
+    Complex *ret;
+    ret->real = c1.real - c2.real;
+    ret->imag = c1.imag - c2.imag;
+    return *ret;
+}
+Complex& operator*(const Complex &c1, const Complex &c2) {
+    Complex *ret;
+    ret->real = c1.real * c2.real;
+    ret->imag = c1.imag * c2.imag;
+    return *ret;
+}
+Complex& operator/(const Complex &c1, const Complex &c2) {
+    Complex *ret;
+    ret->real = (c1.real * c2.real + c1.imag * c2.imag) * 1.0 / (c2.real * c2.real + c2.imag * c2.imag) ;
+    ret->imag = (c1.imag * c2.real - c1.real * c2.imag) * 1.0 / (c2.real * c2.real + c2.imag * c2.imag);
+    return *ret;
+}
+#define min 0.0000001
+bool operator==(const Complex &c1, const Complex &c2) {
+    if (c1.real - c2.real < min && c1.imag - c2.imag < min) return true;
+    else return false;
+}
+bool operator!=(const Complex &c1, const Complex &c2) {
+    if (c1.real - c2.real < min && c1.imag - c2.imag < min) return false;
+    else return true;
 }
 
 int main() {
     Complex c1(1,2);
     Complex c2(3,4);
     Complex c3;
-    //c3 = c1 + c2;
-    c3 = operator+(c1, c2);
+    c3 = c1 + c2;
     cout << c3.getR() << "+" << c3.getI() << "i" << endl;
+    c3 = c1 - c2;
+    cout << c3.getR();
+    if (c3.getI() > 0) cout << "+";
+    cout << c3.getI() << "i" << endl;
+    c3 = c1 * c2;
+    cout << c3.getR() << "*" << c3.getI() << "i" << endl;
+    c3 = c1 / c2;
+    cout << c3.getR() << "/" << c3.getI() << "i" << endl;
+    if (c1 == c2)
+    cout << "equal" << endl;
+    if (c1 != c2) 
+    cout << "not equal" << endl;
     return 0;
 }
