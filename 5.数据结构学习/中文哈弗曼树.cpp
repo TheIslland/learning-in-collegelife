5bd4f971825a1ca3ce1cfd79203648ca9a4c8af4
TheIsland <861436930@qq.com>
2019-01-22 21:05:34 +0800

    中文哈弗曼树.cpp

--- 5.数据结构学习/中文哈弗曼树.cpp
+++ 5.数据结构学习/中文哈弗曼树.cpp
@@ -4,7 +4,6 @@
 	> Mail: 861436930@qq.com
 	> Created Time: 2018年10月29日 星期一 18时17分00秒
  ************************************************************************/
-
 #include <iostream>
 #include <algorithm>
 #include <queue>
@@ -12,9 +11,95 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#define BASE_CNT 26
+#define BASE '0'
 
 using namespace std;
+
 typedef long long ll;
+
+typedef struct charactor { 
+    char key[3]; 
+    ll count; 
+    struct charactor *next; 
+} Charactor; 
+
+Charactor *add(char *c, Charactor *head, int *length) { 
+    int flag = 1; 
+    Charactor *p = head; 
+    while(p != NULL && flag == 1) { 
+        if (strcmp(p->key, c) == 0) 
+            flag = 0; 
+        else 
+            p = p->next; 
+    } 
+    if(flag == 0) {
+        *length += 1;
+        p->count++; 
+    }
+    else { 
+        p = head; 
+        head = (Charactor *)malloc(sizeof(Charactor)); 
+        strcpy(head->key,c); 
+        head->count = 1; 
+        head->next = p; 
+    } 
+    return head; 
+} 
+
+typedef struct TrieNode {
+    int flag;
+    struct TrieNode **next;
+} TrieNode;
+
+TrieNode *getNewTrieNode() {
+    TrieNode *p = (TrieNode *)malloc(sizeof(TrieNode));
+    p->next = (TrieNode **)malloc(sizeof(TrieNode *) * 2);
+    for (int i = 0; i < 2; i++) {
+        p->next[i] = NULL;
+    }
+    p->flag = 0;
+    return p;
+}
+
+int TrieNode_insert(TrieNode *node, const char *str) {
+    TrieNode *p = node;
+    int len = strlen(str);
+    for (int i = 0; str[i]; i += 3) {
+        for (int j = 0; j < 3; j++) {
+            int num = str[i + j];
+            while (num) {
+                int x = num & 1;
+                if (p->next[x] == NULL) {
+                    p->next[x] = getNewTrieNode();
+                }
+                p = p->next[x];
+                num /= 2;
+            }
+        }
+        p->flag += 1;
+    }
+}
+
+int TrieNode_search(TrieNode *node, const char *str) {
+    TrieNode *p = node;
+    int len = strlen(str);
+    for (int i = 0; str[i]; i += 3) {
+        for (int j = 0; j < 3; j++) {
+            int num = str[i + j];
+            while (num) {
+                int x = num & 1;
+                if (p->next[x] == NULL) {
+                    return 0;
+                }
+                p = p->next[x];
+                num /= 2;
+            }
+        }
+    }
+    return p->flag;
+}
+
 typedef struct Node {
     ll freq;
     char *name;
@@ -78,10 +163,16 @@ Node *build_node(priority_queue<Node> &q) {
     return ret;
 }
 
-void extract_node(Node *root, char (*code)[30], int k, char *buff) {
+typedef struct Note {
+    char *name;
+    char *buff;
+}Note;
+
+void extract_node(Node *root, Note *code, int k, char *buff) {
     buff[k] = 0;
     if (root->ind >= 0) {
-        strcpy(code[root->ind], buff);
+        code[root->ind].buff = strdup(buff);
+        code[root->ind].name = strdup(root->name);
         return ;
     }
     buff[k] = '0';
@@ -100,26 +191,38 @@ void output(Node *root) {
     return;
 }
 
+
 int main() {
-    int n ; 
+    TrieNode *Trie = getNewTrieNode();
+    char str[1000][30];
+    Charactor *head = NULL; 
+	int n, length = 0;
     scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", str[i]);
+        for (int j = 0; j < strlen(str[i]); j += 3) {
+            char *temp = (char *)malloc(sizeof(char) * 3);
+            strncpy(temp, str[i] + j, 3);
+            head = add(temp, head, &length);    
+        }
+    }
     Node *root;
     priority_queue<Node> q;
-    char str[20][1000];
-    for (int i = 0 ; i < n; i++) {
+    for (int i = 0 ; head != NULL ; i++) {
         ll freq;
         Node *temp;
-        scanf("%s%lld", str[i], &freq);
-        temp = getNewNode(str[i], freq, i);
+        temp = getNewNode(head->key, head->count, i);
+        cout << temp->name << " " << temp->freq << endl;
         q.push(*temp);
+        head = head->next;
     }
     root = build_node(q);
     output(root);
-    char code[256][30] = {0}, buff[30];
+    Note code[length];
+    char buff[40];
     extract_node(root, code, 0, buff);
-    for (int i = 0; i < n; i++) {
-        if (str[i][0] == 0) continue;
-        cout << str[i] << ": " << code[i] << endl;
+    for (int i = 0; i <= n; i++) {
+        cout << code[i].name << ": " << code[i].buff << endl;
     }
     return 0;
 }
