5186a29306ec801ad32abaf0f8596570a9904688
TheIsland <861436930@qq.com>
2019-03-01 19:26:44 +0800

    方程的解数.cpp

--- 20.蓝桥/方程的解数.cpp
+++ 20.蓝桥/方程的解数.cpp
@@ -4,7 +4,7 @@
 	> Mail: 861436930@qq.com
 	> Created Time: 2019年03月01日 星期五 18时02分05秒
  ************************************************************************/
-#include <iostream>
+/*#include <iostream>
 #include <algorithm>
 #include <string>
 #include <vector>
@@ -45,6 +45,55 @@ int main() {
         cin >> k[i] >> p[i];
     }
     dfs(1, 0);
+    cout << ans << endl;
+    return 0;
+}
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int n, m, ans = 0, sum = 0;
+
+int k[155], p[155], temp[160][5], value[15];
+
+void init() {
+    memset(temp, 1, sizeof(temp));
+	for (int i = 1; i <= 150; i++) {
+		for (int j = 1; j <= 4; j++) {
+			temp[i][j] = i * temp[i][j - 1];
+        }
+    }
+}
+
+void dfs(int i) {
+    if (i == n + 1) {
+		sum = 0;
+        for (int i = 1; i <= n; i++)
+        sum += k[i] * temp[value[i]][p[i]];
+        if (sum == 0) ans++;
+        return ;
+    }
+    for (int j = 1; j <= m; j++) {
+    	value[i] = j;
+        dfs(i + 1);
+    }
+    return ;
+}
+
+int main() {
+    init();
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> k[i] >> p[i];
+    }
+    dfs(1);
     cout << ans << endl;
     return 0;
 }
