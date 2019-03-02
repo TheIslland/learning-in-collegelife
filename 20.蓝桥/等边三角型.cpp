f509f245f1a4bccb7be3804ca8fccc63caab236f
TheIsland <861436930@qq.com>
2019-03-02 12:31:17 +0800

    等边三角型.cpp

---
+++ 20.蓝桥/等边三角型.cpp
@@ -0,0 +1,51 @@
+/*************************************************************************
+	> File Name: 等边三角型.cpp
+	> Author:TheIslland 
+	> Mail: 861436930@qq.com
+	> Created Time: 2019年03月02日 星期六 08时53分26秒
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+ 
+using namespace std;
+
+int n, flag = 0, num, sum = 0, len = 0;
+int a[40];
+int temp[4];
+
+void dfs(int i, int num) {
+    temp[num] += a[i];
+    if (temp[0] > len || temp[1] > len || temp[2] > len) {
+        temp[num] -= a[i];
+        return ;
+    }
+    if (temp[0] == len && temp[1] == len && temp[2] == len) {
+        flag = 1;
+        return ;
+    }
+    if (i + 1 < n) {
+        if (!flag) dfs(i + 1, 0);
+        if (!flag) dfs(i + 1, 1);
+        if (!flag) dfs(i + 1, 2);
+    }
+    temp[num] -= a[i];
+    return ;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        len += a[i];
+    }
+    len /= 3;
+    dfs(0, 0);
+    if (flag) cout << "yes" << endl;
+    else cout << "no" << endl;
+    return 0;
+}
