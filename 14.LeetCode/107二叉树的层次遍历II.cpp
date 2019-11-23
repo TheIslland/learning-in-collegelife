f174946d15e9f3854f7ae6a8682071bc119b47dd
TheIslland <861436930@qq.com>
2019-11-23 14:12:51 +0800

    14.LeetCode/

--- 14.LeetCode/107二叉树的层次遍历II.cpp
+++ 14.LeetCode/107二叉树的层次遍历II.cpp
@@ -1,8 +1,8 @@
 /************************************************************
-    File Name : 102二叉树层次遍历.cpp
+    File Name : 107二叉树的层次遍历II.cpp
     Author: TheIslland
     Mail: voidland@163.com, 861436930@qq.com
-    Created Time: 2019-11-23 星期六 11:04:00
+    Created Time: 2019-11-23 星期六 14:12:14
 ************************************************************/
 #include <iostream>
 #include <algorithm>
@@ -31,16 +31,18 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> levels;
-    void Sub_Node(TreeNode *root, int level) {
+    void Sub_Node(TreeNode* root, int level) {
         if (!root) return;
         if (levels.size() == level) levels.push_back(vector<int>());
         levels[level].push_back(root->val);
         if (root->left != NULL) Sub_Node(root->left, level + 1);
         if (root->right != NULL) Sub_Node(root->right, level + 1);
+        return ;
     }
-    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
         if (!root) return levels;
         Sub_Node(root, 0);
+        reverse(levels.begin(), levels.end());
         return levels;
     }
 };
